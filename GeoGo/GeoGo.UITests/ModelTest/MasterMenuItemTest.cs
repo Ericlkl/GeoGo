ca178f939c5f50479de6229c136a37e176c7baa8
Eric Lee <Raidenlkl@gmail.com>
2018-10-30 23:40:37 +1000

    MasterItem , UserLocationTest added

---
+++ GeoGo/GeoGo.UITests/ModelTest/MasterMenuItemTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using GeoGo.Model;
+using System;
+using Xamarin.Forms;
+
+namespace GeoGo.UITests.ModelTest
+{
+    [TestFixture()]
+    public class MasterMenuItemTest
+    {
+        MasterMenuItem item;
+
+        [SetUp]
+        public void SetUp(){
+            item = new MasterMenuItem("Title", "Source", Color.Blue, typeof(Page) );
+        }
+
+        [Test()]
+        public void MustHaveTitle()
+        {
+            Assert.AreEqual(item.Title,"Title");
+        }
+
+        [Test()]
+        public void MustHaveSource()
+        {
+            Assert.AreEqual(item.IconSource, "Source");
+        }
+
+        [Test()]
+        public void MustHaveColor(){
+            Assert.IsInstanceOf(typeof(Color), item.BackgroundColor);
+        }
+
+        [Test()]
+        public void MustHavePage()
+        {
+            Assert.IsInstanceOf(typeof(Page), item.targetType);
+        }
+    }
+}
---
+++ GeoGo/GeoGo.UITests/ModelTest/UserLocationTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using GeoGo.Model;
+
+namespace GeoGo.UITests.ModelTest
+{
+    [TestFixture()]
+    public class UserLocationTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            UserLocation.UpdateMyCoordinate();
+        }
+
+        [Test()]
+        public void CanUpdateUserCurrentCoordinateTest()
+        {
+            UserLocation.UpdateMyCoordinate();
+            Assert.IsNotNull(UserLocation.Latitude);
+            Assert.IsNotNull(UserLocation.Longitude);
+        }
+    }
+}
